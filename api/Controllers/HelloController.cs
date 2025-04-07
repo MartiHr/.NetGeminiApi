f29a007f46c04adad838d884a5ef0161d60b0931
MartiHr <81310884+MartiHr@users.noreply.github.com>
2025-04-07 23:54:37 +0300

    Add CORS

---
+++ api/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from .NET api");
+        }
+    }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -1,15 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Add CORS configuration
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")  // Allow requests from React frontend
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
+// Use CORS before other middlewares
+app.UseCors();
+
 // Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
